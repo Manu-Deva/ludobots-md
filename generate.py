aba513f178cce91f60eb82e0a93a619b78dff18c
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-07 17:02:26 -0600

    halfway through E

--- generate.py
+++ generate.py
@@ -1,6 +1,5 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
 length = 1
 width = 1
 height = 1
@@ -8,10 +7,21 @@ x = 0
 y = 0.5
 z = 0
 n = 0
-while n < 10:
-    for p in range(5):
-        for a in range(5):
-            pyrosim.Send_Cube(
-                name="Box", pos=[z+a, x+p, y+n], size=[(0.9**n)*length, (0.9**n)*width, (0.9**n)*height])
-    n += 1
-pyrosim.End()
+
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(
+        name="Box", pos=[z, x, y], size=[length, width, height])
+    pyrosim.End()
+
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Robot", pos=[z, x, y], size=[
+                      length, width, height])
+    pyrosim.End()
+
+
+Create_World()
+Create_Robot()
