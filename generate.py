fb5c87b3fb2630ca4e5632342577f02f769d78b2
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-07 15:48:57 -0600

    error starting again

--- generate.py
+++ generate.py
@@ -1,5 +1,8 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+length = 1
+width = 1
+height = 1
+pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[length, width, height])
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,14 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
+
 for x in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
