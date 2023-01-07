2e6ccaddf47646807aff68bf4f6457984b4659a5
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-07 16:38:16 -0600

    part of 25 towers done

--- generate.py
+++ generate.py
@@ -1,11 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0.5
 z = 0
-pyrosim.Send_Cube(name="Box", pos=[z, x, y], size=[length, width, height])
+n = 0
+while n < 10:
+    for p in range(5):
+        pyrosim.Send_Cube(
+            name="Box", pos=[z, x+p, y+n], size=[(0.9**n)*length, (0.9**n)*width, (0.9**n)*height])
+    n += 1
 pyrosim.End()
