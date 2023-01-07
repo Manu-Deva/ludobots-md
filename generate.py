4120e6a8f1805d1a8f97a385f0e1584149016ef3
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-07 15:55:28 -0600

    error stopped

--- generate.py
+++ generate.py
@@ -1,8 +1,5 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-length = 1
-width = 1
-height = 1
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[length, width, height])
+pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
 pyrosim.End()
