e361524fca221e3667338c1e71c8e523e6e72f60
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-07 17:21:16 -0600

    step 37 of E done

--- generate.py
+++ generate.py
@@ -12,14 +12,27 @@ n = 0
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(
-        name="Box", pos=[z, x, y], size=[length, width, height])
+        name="Box", pos=[-10, -10, 1], size=[length, width, height])
     pyrosim.End()
 
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Robot", pos=[z, x, y], size=[
+    pyrosim.Send_Cube(name="Link0", pos=[0, 0, 0.5], size=[
                       length, width, height])
+
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0",
+                       child="Link1", type="revolute", position=[0, 0, 1.0])
+
+    pyrosim.Send_Cube(
+        name="Link1", pos=[0, 0, 0.5], size=[length, width, height])
+
+    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1",
+                       child="Link2", type="revolute", position=[0, 0, 1.0])
+
+    pyrosim.Send_Cube(
+        name="Link2", pos=[0, 0, 0.5], size=[length, width, height])
+
     pyrosim.End()
 
 
