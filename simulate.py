dcf8dde931a370acc3234f2be5d2882b1fb24e89
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-09 15:42:46 -0600

    Revert "added sensor"
    This reverts commit 403f72dd35e87ae7ad3f245513156bfcff6b4313.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,5 @@
 import pybullet as p
 import pybullet_data
-import pyrosim.pyrosim as pyrosim
 import time
 
 physicsClient = p.connect(p.GUI)
@@ -11,11 +10,8 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
-pyrosim.Prepare_To_Simulate(robotId)
-
 for x in range(1000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
     time.sleep(1/30)
+    print(x)
 p.disconnect()
