6d00ec3029367faa2467d1f7bf115ddc7d0683c9
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-09 15:38:41 -0600

    breh

--- simulate.py
+++ simulate.py
@@ -11,11 +11,11 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
-pyrosim.Prepare_To_Simulate(robotId)
+# pyrosim.Prepare_To_Simulate(robotId)
 
 for x in range(1000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    # backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    # print(backLegTouch)
     time.sleep(1/30)
 p.disconnect()
