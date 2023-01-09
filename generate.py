cd88859ce8f017f9a61a3a4a01270a5bd5acd737
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-09 13:24:02 -0600

    final commit for task E

--- generate.py
+++ generate.py
@@ -35,30 +35,6 @@ def Create_Robot():
     pyrosim.Send_Cube(
         name="FrontLeg", pos=[0, 0.5, -0.5], size=[length, width, height])
 
-    # pyrosim.Send_Joint(name="Link2_Link3", parent="Link2",
-    #                    child="Link3", type="revolute", position=[0, 0.5, 0.5])
-
-    # pyrosim.Send_Cube(
-    #     name="Link3", pos=[0, 0.5, 0], size=[length, width, height])
-
-    # pyrosim.Send_Joint(name="Link3_Link4", parent="Link3",
-    #                    child="Link4", type="revolute", position=[0, 1, 0])
-
-    # pyrosim.Send_Cube(
-    #     name="Link4", pos=[0, 0.5, 0], size=[length, width, height])
-
-    # pyrosim.Send_Joint(name="Link4_Link5", parent="Link4",
-    #                    child="Link5", type="revolute", position=[0, 0.5, -0.5])
-
-    # pyrosim.Send_Cube(
-    #     name="Link5", pos=[0, 0, -0.5], size=[length, width, height])
-
-    # pyrosim.Send_Joint(name="Link5_Link6", parent="Link5",
-    #                    child="Link6", type="revolute", position=[0, 0, -1])
-
-    # pyrosim.Send_Cube(
-    #     name="Link6", pos=[0, 0, -0.5], size=[length, width, height])
-
     pyrosim.End()
 
 
