df158463e799fdd7805cad984faa9d6cb76179e6
Manu-Deva <manudeva2025@u.northwestern.edu>
2023-01-15 20:54:33 -0600

    everything seems to be working

--- simulate.py
+++ simulate.py
@@ -8,8 +8,8 @@ import math
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
 p.setGravity(0, 0, -9.8)
+
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
@@ -20,18 +20,26 @@ frontLegSensorValues = np.zeros(1000)
 
 
 for x in range(1000):
-    time.sleep(1/30)
+    time.sleep(1/90)
     p.stepSimulation()
     backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link(
         "FrontLeg")
     pyrosim.Set_Motor_For_Joint(
-        bodyIndex="robot",
-        jointName="Torso_BackLeg",
+        bodyIndex=robotId,
+        jointName='Torso_BackLeg',
         controlMode=p.POSITION_CONTROL,
-        targetPosition=0.0,
+        targetPosition=math.pi/2,
         maxForce=500
     )
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName='Torso_FrontLeg',
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=-math.pi/2,
+        maxForce=500
+    )
+
 
 p.disconnect()
 print(backLegSensorValues)
